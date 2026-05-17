2a9fa8f8134eb9f3359f336c8e13e399f619b3b9
anjalei <anjaleiirnair@gmail.com>
2026-05-17 12:36:51 +0530

    Javascript inside jsx

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,10 +1,18 @@
 function ExpenseItem (){
-return (
-  <>
-    <p>food-$5</p>
-    <p>petrol-$20</p>
-    <p>movie-$10</p>
-  </>
-)
+  const expenseDate = new Date(2026, 7, 15).toISOString();
+  const expenseTitle ="Insurance";
+  const expenseAmount = 50;
+  const expenseLocation = "Bangalore"
+  return (
+    <div className="expense-item">
+      <div>{expenseDate}</div>
+      <div className='expense-item__location'>{expenseLocation}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
+
 export default ExpenseItem;
