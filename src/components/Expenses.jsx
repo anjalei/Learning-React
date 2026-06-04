a383cfa121cede2c4d0cbe77299aa086d592a797
anjalei <anjaleiirnair@gmail.com>
2026-06-04 11:55:03 +0530

    Wrap components

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,15 +1,10 @@
 import "./Expenses.css"
 import ExpenseItem from "./ExpenseItem";
-const expenses = [
-  { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
-  { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10 },
-  { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1 },
-  { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200 },
-];
-function Expenses() {
+import Card from "../ui/Card";
+function Expenses(props) {
   return (
-    <div>
-      {expenses.map((expense) => (
+    <Card className="expenses">
+      {props.expenses.map((expense) => (
           <ExpenseItem
           key = { expense.id }
           date = { expense.date }
@@ -17,7 +12,7 @@ function Expenses() {
           price={expense.price}
         />
     ))}
-    </div>
+    </Card>
   );
 }
 export default Expenses;
