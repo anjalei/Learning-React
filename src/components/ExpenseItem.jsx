856a39c2806f17fb06981339d9e8d03f3d834f80
anjalei <anjaleiirnair@gmail.com>
2026-06-04 11:58:10 +0530

    Event Handler

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -2,6 +2,9 @@
 import ExpenseDate from "./ExpenseDate";
 import Card from "../ui/Card";
  function ExpenseItem (props){ 
+  function buttonClickHandler(event){
+    console.log("Title Changed");
+  }
   return (
     <Card className="expense-item">
     <ExpenseDate date={props.date} ></ExpenseDate>
@@ -9,6 +12,7 @@ import Card from "../ui/Card";
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
         </div>
+        <button onClick={buttonClickHandler}>Change Title</button>
         </Card>
   );
 }
